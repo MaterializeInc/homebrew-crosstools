b6345bef756711c000bea7446fae1526b09783e3
Nikhil Benesch <882976+benesch@users.noreply.github.com>
2021-12-22 22:59:59 +0000

    x86_64-unknown-linux-gnu: add 0.1.0 bottle.

--- Formula/x86_64-unknown-linux-gnu.rb
+++ Formula/x86_64-unknown-linux-gnu.rb
@@ -6,6 +6,12 @@ class X8664UnknownLinuxGnu < Toolchain
   desc "x86_64 Linux GNU toolchain"
   version "0.1.0"
 
+  bottle do
+    root_url "https://github.com/MaterializeInc/homebrew-crosstools/releases/download/x86_64-unknown-linux-gnu-0.1.0"
+    sha256 cellar: :any, arm64_big_sur: "bb23a1e08218c0acdfe8701c0a0aea5a76adb010610c941976a0368939debbd4"
+    sha256 cellar: :any, catalina:      "8290d01510eb2a1bb8c6f4c97e88ad73e1536ffb9b3e0eb2417b25cb294d114d"
+  end
+
   defconfig <<~EOS
     CT_ARCH_64=y
     CT_ARCH_X86=y
