327bb4f4481f1ece88ed97edaf0d39ffc8c39d6e
Nikhil Benesch <882976+benesch@users.noreply.github.com>
2021-12-22 22:59:59 +0000

    aarch64-unknown-linux-gnu: add 0.1.0 bottle.

--- Formula/aarch64-unknown-linux-gnu.rb
+++ Formula/aarch64-unknown-linux-gnu.rb
@@ -6,6 +6,12 @@ class Aarch64UnknownLinuxGnu < Toolchain
   desc "aarch64 Linux GNU toolchain"
   version "0.1.0"
 
+  bottle do
+    root_url "https://github.com/MaterializeInc/homebrew-crosstools/releases/download/aarch64-unknown-linux-gnu-0.1.0"
+    sha256 cellar: :any, arm64_big_sur: "e9a7883db4e4bca222dacfe6576b46318ebe949f4352aa061c8b5e80d6479bea"
+    sha256 cellar: :any, catalina:      "b9128e88b58f1afb20092c9478c7fc3d9daf935baeda344a8eae18f01231db4f"
+  end
+
   defconfig <<~EOS
     CT_ARCH_64=y
     CT_ARCH_ARM=y
