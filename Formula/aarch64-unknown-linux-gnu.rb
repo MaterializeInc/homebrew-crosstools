5ab94b86a8ed2ddaeacb3ac7dc55f435edced2d8
Dennis Felsing <dennis@felsing.org>
2026-05-11 00:50:30 +0000

    aarch64-unknown-linux-gnu: add 0.2.0 bottle.

--- Formula/aarch64-unknown-linux-gnu.rb
+++ Formula/aarch64-unknown-linux-gnu.rb
@@ -6,6 +6,11 @@ class Aarch64UnknownLinuxGnu < Toolchain
   desc "aarch64 Linux GNU toolchain"
   version "0.2.0"
 
+  bottle do
+    root_url "https://github.com/MaterializeInc/homebrew-crosstools/releases/download/aarch64-unknown-linux-gnu-0.2.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2dc7ae811c37881c0a441029104eed0a07e5f81270c66c7c26a44442d01542ea"
+  end
+
   defconfig <<~EOS
     CT_ARCH_ARM=y
     CT_ARCH_64=y
