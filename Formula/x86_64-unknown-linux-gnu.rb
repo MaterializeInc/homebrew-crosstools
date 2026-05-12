242c64dea976f21bdfe68b261c4a24a053c3f447
Dennis Felsing <dennis@felsing.org>
2026-05-12 00:39:46 +0000

    x86_64-unknown-linux-gnu: add 0.2.1 bottle.

--- Formula/x86_64-unknown-linux-gnu.rb
+++ Formula/x86_64-unknown-linux-gnu.rb
@@ -6,6 +6,11 @@ class X8664UnknownLinuxGnu < Toolchain
   desc "x86_64 Linux GNU toolchain"
   version "0.2.1"
 
+  bottle do
+    root_url "https://github.com/MaterializeInc/homebrew-crosstools/releases/download/x86_64-unknown-linux-gnu-0.2.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65e9d7b24e9584def1f5c2c4c689257fb358570680647bc292616bf9bd28998c"
+  end
+
   defconfig <<~EOS
     CT_ARCH_X86=y
     CT_ARCH_64=y
